f73ecdd93415bca140dceacac6fe7c63f6377073
krystleL21 <54385637+krystleL21@users.noreply.github.com>
2025-04-14 00:51:21 -0400

    Added the Ai API integration

--- src/quoteaigen.js
+++ src/quoteaigen.js
@@ -1,13 +1,25 @@
-function generatePoem(event) {
+function displayQuote(response) {
+    new Typewriter("#quotePL", {
+        strings: response.data.answer,
+        autoStart: true,
+        delay: 1,
+        cursor: "",
+      });
+}
+
+
+
+function generateQuote(event) {
         event.preventDefault();
-      
-        new Typewriter("#quotePL", {
-          strings: "Keep your eyes on the stars, and your feet on the ground",
-          autoStart: true,
-          delay: 1,
-          cursor: "",
-        });
+   
+        let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "b68t905a51397a01bf1fcdf4ao4dde24";
+  let prompt = `Generate a quote about ${instructionsInput.value}`;
+  let context = "You are a expert in quote writing and love to write short quotes. Your mission is to generate a 3 line quotes in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the quote. Sign at the end of the quote with 'SheCodes AI' inside a <strong> element and NOT at the beginning. Do not include 'html' and '''''";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiURL).then(displayQuote);
       }
       
-      let poemFormElement = document.querySelector("#quote-generator-form");
-      poemFormElement.addEventListener("submit", generatePoem);
+      let quoteFormElement = document.querySelector("#quote-generator-form");
+      quoteFormElement.addEventListener("submit", generateQuote);
